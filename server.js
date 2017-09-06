9a51edcbdb4caea83bca93edfce34881b3514066
jmscott <jmscottimd@gmail.com>
2017-09-05 21:23:24 -0400

    mongoose connection
    add handlers for annoying mongoose bug

--- server.js
+++ server.js
@@ -3,15 +3,24 @@ var app = express();
 var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/blogcc');
-mongoose.connect('mongodb://localhost:27017/blogcc', {
-        useMongoClient: true
-    }, function(ignore, connection) {
-        connection.onOpen()
-    }).then(() => {
-        console.log('connected')
-    })
-    .catch(console.error)
+mongoose.connect('mongodb://localhost:27017/blogcc');
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose default connection open to ' + dbURI);
+});
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {
+    console.log('Mongoose default connection error: ' + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose default connection disconnected');
+});
+
+
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
@@ -21,11 +30,11 @@ app.use(bodyParser.urlencoded({
 var PostSchema = mongoose.Schema({
     title: {type: String, required: true},
     body: String,
-    tag:{type:String, enum:['Vacation','Daily','Wow']},
+    tag: {type: String, enum: ['Vacation', 'Daily', 'Wow']},
     posted: {type: Date, default: Date.now}
 });
 
-var PostModel = mongoose.model("PostModel",PostSchema);
+var PostModel = mongoose.model("PostModel", PostSchema);
 app.post("/api/blogpost", createPost);
 
 function createPost(req, res) {
